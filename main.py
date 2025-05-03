3e8d9fd74430f57beb30dc5b38990721ae77450b
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-05-04 02:50:44 +0300

    Add initial Jupyter notebooks and Python scripts for data manipulation with pandas
    - Created `kaggle-pandas.ipynb` to demonstrate basic DataFrame creation and display.
    - Added `main.py` to showcase DataFrame creation from a dictionary and print the result.
    - Introduced `read-file.ipynb` for reading a CSV file containing ramen ratings and displaying the data.
    - Developed `write-file.ipynb` to illustrate saving DataFrames to CSV and JSON formats.

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {
+    "name": ["yehia", "yossef", "yassen"],
+    "age": ["20", "18", "13"]
+}
+
+df = pd.DataFrame(data)
+print(df)  # Add this line to display the DataFrame
