bb1205c680f2a82b7bbba3b6cf2162f1f7c62e9c
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-05-06 00:01:47 +0300

    Remove main.py and add renaming_and_combining Jupyter notebook for data manipulation and analysis

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = {
-    "name": ["yehia", "yossef", "yassen"],
-    "age": ["20", "18", "13"]
-}
-
-df = pd.DataFrame(data)
-print(df)  # Add this line to display the DataFrame
